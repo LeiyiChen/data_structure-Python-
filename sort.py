4239e3c69715bdfdcfc5d618b04ec54d33d741ed
Chenleiy <429719382@qq.com>
2018-07-03 22:27:22 +0800

    update

--- sort.py
+++ sort.py
@@ -2,7 +2,10 @@
 '''
 排序算法实现
 1.插入排序
-2.
+2.希尔排序
+3.选择排序
+4.冒泡排序
+5.归并排序
 
 '''
 def insert_sort(nums):
@@ -28,9 +31,8 @@ def insert_sort(nums):
                     nums.insert(j, tmp)
     return nums
 
+
 #希尔排序
-
-
 def shell_sort(nums):
     l = len(nums)
     dk = l//2
@@ -41,6 +43,55 @@ def shell_sort(nums):
         dk = dk // 2
     return insert_sort(nums)
 
+ #选择排序
+def select_sort(nums):
+    res = []
+    l = len(nums)
+    while 0 < l:
+        res.append(min(nums))
+        nums.remove(min(nums))
+        l -= 1
+    return res
+
+
+#冒泡排序
+def bubble_sort(nums):
+    h = len(nums) - 1
+    l = 0
+    while l < h:
+        i = 0
+        while i < h:
+            if nums[i] > nums[i + 1]:
+                nums[i + 1], nums[i] = nums[i], nums[i + 1]
+            i += 1
+        h -= 1
+    return nums
+
+#归并排序
+def merge(a, b):
+    res = []
+    i = j = 0
+    while i < len(a) and j < len(b):
+        if a[i] < b[j]:
+            res.append(a[i])
+            i += 1
+        else:
+            res.append(b[j])
+            j += 1
+    if i == len(a) and j < len(b):
+        res.extend(b[j:])
+    elif j == len(b) and i < len(a):
+        res.extend(a[i:])
+    return res
+
+def merge_sort(nums):
+    n = len(nums)
+    if n == 1:
+        return nums
+    else:
+        left = merge_sort(nums[:n//2])
+        right = merge_sort(nums[n//2:])
+    return merge(left, right)
 
 
 
